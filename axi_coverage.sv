f33dff0d08747d0b88dae6c904c467f7c3f33f56
Dhirajzen <117260766+Dhirajzen@users.noreply.github.com>
2026-03-06 06:52:36 +0000

    Fixed Coverage

--- axi_coverage.sv
+++ axi_coverage.sv
@@ -3,10 +3,18 @@ class axi_coverage extends uvm_subscriber #(axi_item);
 
   covergroup cg with function sample(axi_item t);
     option.per_instance = 1;
+
     dir_cp   : coverpoint t.dir;
     burst_cp : coverpoint t.burst;
-    size_cp  : coverpoint t.size { bins b[] = {0,1,2}; bins illegal = default; }
-    len_cp   : coverpoint t.len  { bins small[] = {0,1,3}; bins med[] = {7}; bins big[] = {15}; }
+    size_cp  : coverpoint t.size { bins legal[] = {0,1,2}; bins illegal = default; }
+
+    // rename bins to avoid keywords like small/medium/large
+    len_cp   : coverpoint t.len  {
+      bins len_s[] = {0,1,3};
+      bins len_m[] = {7};
+      bins len_l[] = {15};
+    }
+
     cross dir_cp, burst_cp, size_cp, len_cp;
   endgroup
 
